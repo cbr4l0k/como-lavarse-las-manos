bc9dedb48d276ea053b92746b0d03d7648fe8637
DaveAlsina <davealsina@gmail.com>
2023-08-29 17:03:27 -0500

    add, added json grammar to daniel's code

--- src/LLMManager.py
+++ src/LLMManager.py
@@ -10,13 +10,16 @@ class LLM:
         self.Llama_model = None
         self.llm_chain = None
         self.prompts = {
-            "t1": """Assistant is designed to recieve a python file, identify which dependencies the file
-    uses and a brief explanation of what the file contains. Assistant must return a json format like this:
-    "dependencies": [],
-    "explanation": ""
+            "t1": """Assistant is designed to receive a python file, identify which dependencies the file
+    uses and a brief explanation of what the file contains. Assistant must return a json with this fields:
+    
+    "dependencies": [list of dependencies names, this are the external imports or packages the code uses],
+    "explanation": 'short code explanation highlighting main features, key classes and functions.'
+   
+     ""
 
-    File recived: {code}"""
-        }
+    For this given file generate the json ONLY, File received: {code}"""
+    }
 
     def load_model(self) -> LlamaCpp:
         """Loads LLama model"""
@@ -39,18 +42,25 @@ class LLM:
 
 
 def main():
+
+    average_number_of_tokens_per_sentence = 27
+    desired_number_of_sentences_per_file = 30
+    max_tokens = desired_number_of_sentences_per_file*average_number_of_tokens_per_sentence
+
     llm = LLM({
-        "model_path": "/home/dleyvacastro/Documents/devsavant/Langchain/model.bin",
-        "temperature": 0,
-        "max_tokens": 500,
-        "n_gpu_layers": 200,
+        "model_path": "/home/lok/devsavant/langchain/como-lavarse-las-manos-master/model.bin",
+        "temperature": 0.1,
+        "max_tokens": max_tokens,
+        "n_gpu_layers": 400,
         "n_batch": 4096,
         "top_p": 1,
         "callback_manager": CallbackManager([StreamingStdOutCallbackHandler()]),
         "verbose": True,
+        "grammar_path": "/home/lok/devsavant/langchain/como-lavarse-las-manos-master/json.gbnf"
     })
     llm.load_chain()
-    with open("Arquitectura/Calculator.py", "r") as f:
+
+    with open("/home/lok/devsavant/langchain/como-lavarse-las-manos-master/Arquitectura/Calculator.py", "r") as f:
         code = f.read()
 
     response = llm.llm_chain.run(code)

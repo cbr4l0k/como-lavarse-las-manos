764e9594d1657c7f65fc5a7af92798048110b024
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2023-09-10 11:58:29 -0500

    hotfix

--- src/Report.py
+++ src/Report.py
@@ -352,7 +352,7 @@ class Report:
                 "w") as f:
             json.dump(self.report, f, indent=4)
 
-        with open(f"{OUTPUTS_PATH}finalreport.json", "w") as f
+        with open(f"{OUTPUTS_PATH}finalreport.json", "w") as f:
             json.dump(self.report, f, indent=4)
 
 

c35f670d0ec97bcd8d324d2f00386d90ffd7bc9d
DaveAlsina <davealsina@gmail.com>
2023-09-10 14:01:56 -0500

    Update FileHandler.py

--- src/FileHandler.py
+++ src/FileHandler.py
@@ -30,29 +30,17 @@ class FileHandler:
         
         Methods:
         ----------
-        - load_json_report(json_path: str)
-            Loads the json report from the given path.
+
+        - chunk_document: List[Document]
+            This function chunks a given block of code taking into account the semantic categories given in a language
+            by considering it's syntax, from it recursively tries to divide each chunk into one or many of the desired
+            chunk size, this does not guarantee that they all have the same size, but they should be close.
+            Also considers the chunk overlap, which allows to have a bit of the previous information available.
         
-        - generate_json_report(project_path: str)
-            Generates the json report for the given project path.
-
-        - complete_json_report()
-            Completes the json report by adding the chunks to the files.
-        
-        - read_files_from_project_tree()
-            Reads the files from the project tree.
-
-        - save_response_for_file(filename: str, response: str, gid: int)
-            Saves the response for the given file, in the given path.
-
-        - from_filename_to_lang(filename: str)
-            Infers the language from the filename.
-
-        - chunk_document(filename_full_path: str, code: str, chunk_size: int, chunk_overlap: int = 0)
-            Chunks the given document into the given chunk size, with the given overlap.
-
-        - get_responses(responses_dir_path: str)
-            Returns an iterator for the responses directory.
+        - from_filename_to_lang: Language
+            This function is supposed to take the filename and infer the language by taking the last part of the name
+            and then return the Language it corresponds to if any (in the dict of supported LangChain languages)
+            If it does not find it, it will return None.
     """
 
     def __init__(self, json_path: str = None) -> None:
@@ -61,28 +49,6 @@ class FileHandler:
 
         if json_path is not None:
             self.load_json_report(json_path)
-
-    @staticmethod
-    def save_response_for_file(filename: str, response: str, gid: int): # return the response in json format
-        """
-            This function is supposed to save the response of the model for a given file. 
-            As a json file, following the format:
-
-            filename_response.json
-
-            Assuming the filename is a path, it will take the last part of the path, 
-            and use it as the filename for the response.
-        """
-
-        output = f"{OUTPUTS_PATH}/" + filename.split("/")[-1].split(".")[0] + "_response.json"
-
-        with open(output, "w") as f:
-            f.write(response)
-
-        with open(output, 'w') as f:
-            res = json.loads(response)
-            res["gid"] = gid
-            json.dump(res, f, indent=4)
 
     @staticmethod
     def from_filename_to_lang(filename: str):
@@ -135,47 +101,3 @@ class FileHandler:
                                                                        language=lang, )
         docs = python_splitter.create_documents([code])
         return docs
-
-
-    def get_responses(self, responses_dir_path: str):
-        """
-            This function returns an iterator which allows reading file by file inside the responses directory.
-            This is useful for aboiding loading all the responses into memory at once, saving memory.
-
-            Args:
-            ----------
-            responses_dir_path: str
-                The path to the responses directory.
-            
-            Returns:
-            ----------
-            Iterator
-                The iterator for the responses directory.
-        """
-        responses = {}
-        for file in os.listdir(responses_dir_path):
-            with open(responses_dir_path + file, "r") as f:
-                jdir = json.load(f)
-                responses[jdir["gid"]] = jdir
-        return responses
-
-    def load_file(self, file_path: str):
-        """
-            This function loads a file from the given path and returns it's contents.
-        """
-        with open(file_path, "r") as f:
-            return f.read()
-
-
-def main():
-    dh = FileHandler()
-    dh.generate_json_report(f"{PROJECTS_PATH}/Arquitectura")
-    dh.read_files_from_project_tree()
-    dh.complete_json_report()
-
-
-
-
-
-if __name__ == "__main__":
-    main()
